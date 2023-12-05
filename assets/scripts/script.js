affedc18d645026f387ea1a330869a91ac2791ed
Madeleine <mgleahy@nic.edu>
2023-12-04 18:33:32 -0800

    started the javascript

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,4 +1,4 @@
-// HAMBURGER MENU
+//-------- HAMBURGER MENU----------
 
 // create a var
 const hamburger = document.querySelector('.hamburger');
@@ -16,3 +16,19 @@ list.classList.toggle('active')
     // the list needs to pop down
    
 // when the hamburger is clicked again the list needs to close
+
+
+// --------dark and light mode-----------
+// make a var for the button
+const button = document.querySelector('.dark_mode_button')
+
+// create an event listener that says when the button is clicked it toggles a class
+
+button.addEventListener('click',function (){
+    const text = document.querySelector('.dark_mode_text');
+    button.classList.toggle('dark_mode_text');
+    
+    text.classList.toggle('active')
+            
+    
+    })
