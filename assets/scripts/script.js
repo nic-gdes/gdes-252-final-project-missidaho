3377cf0a9fb9f4dd394b4ef7187e69212cc6af97
Madeleine <mgleahy@nic.edu>
2023-12-05 15:47:49 -0800

    got the modal finished
    started dark and light mode again

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -22,13 +22,44 @@ list.classList.toggle('active')
 // make a var for the button
 const button = document.querySelector('.dark_mode_button')
 
+
+
 // create an event listener that says when the button is clicked it toggles a class
 
-button.addEventListener('click',function (){
-    const text = document.querySelector('.dark_mode_text');
-    button.classList.toggle('dark_mode_text');
+button.addEventListener('click', function() {
+    const mode = document.querySelectorAll('.dark_mode')
+    mode.classList.toggle('.dark_mode')
     
-    text.classList.toggle('active')
-            
+}
     
-    })
+    
+)
+
+
+    // ---modal----
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
